cc725d381fe547dbe05070afb7e2405085cd4352
Biplav Adhikary <biplov@live.com>
2021-10-14 13:39:34 +0530

    Removed Unused Files

--- src/utils/helper/material-kit-react.ts
+++ src/utils/helper/material-kit-react.ts
@@ -60,7 +60,7 @@ const card = {
 
 const defaultFont = {
   fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-  fontWeight: "300",
+  fontWeight: 300,
   lineHeight: "1.5em"
 };
 
@@ -154,7 +154,7 @@ const title = {
   color: "#3C4858",
   margin: "1.75rem 0 0.875rem",
   textDecoration: "none",
-  fontWeight: "700",
+  fontWeight: 700,
   fontFamily: `"Roboto Slab", "Times New Roman", serif`
 };
 
